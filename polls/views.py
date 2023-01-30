c0ed5b6b4d96d5a31fe34d5656849114905c18a7
avahmed <avahmed@ualberta.ca>
2023-01-30 15:45:38 -0700

    django part 3 - add views: detail, results, and vote

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    response = "Hello, world. You're at the polls index."
+    return HttpResponse(response)
+
+def detail(request, question_id):
+    response = "You're looking at question %s." % question_id
+    return HttpResponse(response)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s." % question_id
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    response = "You're voting on question %s." % question_id
+    return HttpResponse(response)
